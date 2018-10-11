3ace9b2a7e9fa7fe3e1c1dc6d67510d98bed2e6a
mohamedabusrea <mohamedabusrea@gmail.com>
2018-10-11 15:52:16 +0400

    updated and cleaned the APIs

--- api/controllers/usersController.js
+++ api/controllers/usersController.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose'),
-    Users = mongoose.model('Users');
+const mongoose = require('mongoose');
+const Users = mongoose.model('Users');
 
 exports.decodeBasicAuth = (authData) => {
   const header = authData || '';       // get the header
@@ -15,17 +15,12 @@ exports.decodeBasicAuth = (authData) => {
 exports.getUserData = (req, res) => {
   // const basicAuthData = this.decodeBasicAuth(req.headers['authorization']);
 
-  Users.findById(req.params.userId, (err, userData) => {
+  Users.find({_id: req.params.userId}, (err, userData) => {
     err ? res.send(err) : res.json(userData);
   });
 };
 
 exports.setUserData = (req, res) => {
-  /*console.log(req.params);
-   Users.findOneAndUpdate({_id: req.params.userId}, req.body,
-   (err, task) => {
-   err ? res.send(err) : res.json(task);
-   });*/
   const new_user = new Users(req.body);
   new_user.save((err, user) => {
     err ? res.send(err) : res.json(user);
@@ -39,43 +34,6 @@ exports.updateUserData = (req, res) => {
                          (err, task) => {
                            err ? res.send(err) : res.json(task);
                          });
-};
-
-exports.list_all_tasks = function(req, res) {
-  Users.find({}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-exports.create_a_task = function(req, res) {
-  const new_task = new Users(req.body);
-  new_task.save(function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-exports.read_a_task = function(req, res) {
-  Users.findById(req.params.taskId, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
-};
-
-exports.test = function(req, res) {
-  res.json('sssssssssss');
-};
-
-exports.update_a_task = function(req, res) {
-  Users.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
-    if (err)
-      res.send(err);
-    res.json(task);
-  });
 };
 
 exports.delete_a_task = function(req, res) {
--- api/routes/usersRoutes.js
+++ api/routes/usersRoutes.js
@@ -9,11 +9,5 @@ module.exports = function(app) {
      .post(users.updateUserData);
 
   app.route('/getUserData/:userId')
-     .get(users.getUserData)
-     .post(users.create_a_task);
-
-  app.route('/tasks/:taskId')
-     .get(users.read_a_task)
-     .put(users.update_a_task)
-     .delete(users.delete_a_task);
+     .get(users.getUserData);
 };

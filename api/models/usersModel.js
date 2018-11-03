74ba08010676191067df4eeacd94522cbff6f83f
mohamedabusrea <mohamedabusrea@gmail.com>
2018-11-03 14:34:53 +0400

    making scheme typeless

--- api/models/usersModel.js
+++ api/models/usersModel.js
@@ -1,21 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+/*const UserSchema = new Schema({
+ _id: String,
+ displayName: {type: String, trim: true},
+ userName: {type: String, trim: true},
+ email: {type: String, trim: true},
+ image: {type: String, trim: true},
+ routines: Schema.Types.Mixed,
+ workouts: Schema.Types.Mixed,
+ exercises: Schema.Types.Mixed,
+ settings: Schema.Types.Mixed,
+ timestamp: Schema.Types.Mixed,
+ metrics: Schema.Types.Mixed,
+ /!*name: {
+ type: String,
+ required: 'Kindly enter the name of the task',
+ },*!/
+ }, {_id: false});*/
 const UserSchema = new Schema({
-                                _id: String,
-                                displayName: {type: String, trim: true},
-                                userName: {type: String, trim: true},
-                                email: {type: String, trim: true},
-                                image: {type: String, trim: true},
-                                routines: Schema.Types.Mixed,
-                                workouts: Schema.Types.Mixed,
-                                exercises: Schema.Types.Mixed,
-                                settings: Schema.Types.Mixed,
-                                timestamp: Schema.Types.Mixed,
-                                /*name: {
-                                 type: String,
-                                 required: 'Kindly enter the name of the task',
-                                 },*/
+                                any: {},
                               }, {_id: false});
 
 module.exports = mongoose.model('Users', UserSchema);

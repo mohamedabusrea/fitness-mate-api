3cdd4bfd50a894ba8dbdec60ef300722b241d0f9
mohamedabusrea <mohamedabusrea@gmail.com>
2018-11-03 14:40:11 +0400

    adding metrics to scheme

--- api/models/usersModel.js
+++ api/models/usersModel.js
@@ -1,25 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-/*const UserSchema = new Schema({
- _id: String,
- displayName: {type: String, trim: true},
- userName: {type: String, trim: true},
- email: {type: String, trim: true},
- image: {type: String, trim: true},
- routines: Schema.Types.Mixed,
- workouts: Schema.Types.Mixed,
- exercises: Schema.Types.Mixed,
- settings: Schema.Types.Mixed,
- timestamp: Schema.Types.Mixed,
- metrics: Schema.Types.Mixed,
- /!*name: {
- type: String,
- required: 'Kindly enter the name of the task',
- },*!/
- }, {_id: false});*/
 const UserSchema = new Schema({
-                                any: {},
+                                _id: String,
+                                displayName: {type: String, trim: true},
+                                userName: {type: String, trim: true},
+                                email: {type: String, trim: true},
+                                image: {type: String, trim: true},
+                                routines: Schema.Types.Mixed,
+                                workouts: Schema.Types.Mixed,
+                                exercises: Schema.Types.Mixed,
+                                settings: Schema.Types.Mixed,
+                                timestamp: Schema.Types.Mixed,
+                                metrics: Schema.Types.Mixed,
+                                /*name: {
+                                 type: String,
+                                 required: 'Kindly enter the name of the task',
+                                 },*/
                               }, {_id: false});
 
 module.exports = mongoose.model('Users', UserSchema);
